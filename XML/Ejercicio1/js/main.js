50a0ddc5cd3db0bf469b46c24c127280b32b6f93
Sergio Castillo <sergiocastillogarci@gmail.com>
2023-10-01 17:33:12 +0200

    Consigo titulos de libros

--- XML/Ejercicio1/js/main.js
+++ XML/Ejercicio1/js/main.js
@@ -1,19 +1,22 @@
 class Biblioteca {
   constructor() {
     this.subfolders = [];
+    this.nombresLibros = [];
     this.RUTA_BOOKS ="../books";
   }
 
   // Método para agregar nombres de subcarpetas a la lista
-  getRutasBooks() {
+  getBooks() {
     const xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = () => {//TODO sacar de aqui
       if (xhr.readyState === XMLHttpRequest.DONE) {
         if (xhr.status === 200) {
           const response = xhr.responseText;
-          this.subfolders = this.extraerNombresSubcarpetas(response);
-          console.log('Nombres de subcarpetas:', this.subfolders);
+          this.subfolders = this.getSubfoldersNames(response);//Consigo las rutas de cada libro
+          this.nombresLibros=this.getBookNames();//Consigo el nombre de cada libro
+          console.log(this.nombresLibros);
+         
         } else {
           console.error('Error al obtener nombres de subcarpetas:', xhr.status);
         }
@@ -24,7 +27,7 @@ class Biblioteca {
     xhr.send();
   }
   //Metodo para parsear la respuesta de la peticion HTTP y conseguir los nombres de las subcarpetas
-  extraerNombresSubcarpetas(response) {
+  getSubfoldersNames(response) {
     // Analizar la respuesta como HTML
     const parser = new DOMParser();
     const htmlDoc = parser.parseFromString(response, 'text/html');
@@ -41,10 +44,80 @@ class Biblioteca {
 
     return subfolderNames;
   }
+
+  //Metodo para extraer el nombre de los libros
+  async getBookNames() {
+
+    // Extraer los nombres de las subcarpetas
+    const booksNames = [];
+    /*for (let i = 0; i < this.subfolders.length; i++) {
+      const subcarpeta = this.subfolders[i];
+      const nombreLibro = this.getBookName(subcarpeta);
+      booksNames.push(nombreLibro);
+    }*/
+    for (let i = 0; i < this.subfolders.length; i++) {
+      const subcarpeta = this.subfolders[i];
+      const nombreLibro = await this.obtenerNombreLibroDesdeXHTML(subcarpeta);
+      if (nombreLibro) {
+        booksNames.push(nombreLibro);
+      }
+    }
+    return booksNames;
+
+  }
+  //metodo para extraer el nombre de un libro dada su ruta
+  getBookName(ruta) {
+    // Suponemos que el nombre del libro es igual al nombre de la carpeta (sin la extensión .epub)
+    return ruta.replace('.epub', '');
+  }
+  async obtenerNombreLibroDesdeXHTML(subcarpeta) {
+    const archivoXHTML = `../books/${subcarpeta}/OEBPS/content.opf`;
+
+    try {
+      const contenido = await this.leerArchivo(archivoXHTML);
+      const nombreLibro = this.extraerNombreLibroDesdeXHTML(contenido);
+      return nombreLibro;
+    } catch (error) {
+      console.error('Error al leer el archivo XHTML:', error);
+      return null;
+    }
+  }
+
+
+  async leerArchivo(ruta) {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            resolve(xhr.responseText);
+          } else {
+            reject(`Error al leer el archivo: ${xhr.status}`);
+          }
+        }
+      };
+
+      xhr.open('GET', ruta);
+      xhr.send();
+    });
+  }
+  
+  extraerNombreLibroDesdeXHTML(contenido) {
+    // Suponemos que el nombre del libro está en una etiqueta <title>
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(contenido, 'application/xml');
+    const titleElement = doc.querySelector('title');
+
+    if (titleElement && titleElement.textContent) {
+      return titleElement.textContent.trim();
+    } else {
+      console.error('No se pudo encontrar el nombre del libro en el archivo XHTML.');
+      return null;
+    }
+  }
 }
 
 // Crear una instancia de Biblioteca
 const biblioteca = new Biblioteca();
-biblioteca.getRutasBooks();
-// Mostrar los nombres de subcarpetas en la consola
-console.log('Nombres de subcarpetas:', biblioteca.subfolders);
+biblioteca.getBooks();
